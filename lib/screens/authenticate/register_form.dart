5316c1fc6bed57fa605d2d587318b600014b2084
Ockham-Byron <ockham-byron@protonmail.com>
2022-03-01 17:40:39 +0100

    validation register form

--- lib/screens/authenticate/register_form.dart
+++ lib/screens/authenticate/register_form.dart
@@ -1,4 +1,5 @@
 import 'package:book_club/services/auth.dart';
+import 'package:book_club/shared/custom_form_field.dart';
 import 'package:flutter/material.dart';
 
 class RegisterForm extends StatefulWidget {
@@ -53,146 +54,184 @@ class _RegisterFormState extends State<RegisterForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      height: 500,
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Tout commence par un incipit",
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              validator: (value) =>
-                  value!.isEmpty ? "Merci d'indiquer un pseudo" : null,
-              focusNode: fpseudo,
-              controller: _pseudoInput,
-              decoration: fieldDecoration(context, Icons.person, "pseudo"),
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            TextFormField(
-              validator: (value) =>
-                  value!.isEmpty ? "Merci d'indiquer un email" : null,
-              focusNode: fmail,
-              controller: _emailInput,
-              decoration:
-                  fieldDecoration(context, Icons.alternate_email, "courriel"),
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            TextFormField(
-              focusNode: fpassword,
-              controller: _passwordInput,
-              obscureText: _isHidden,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Theme.of(context).canvasColor)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Theme.of(context).focusColor)),
-                prefixIcon: Icon(Icons.lock_outline,
-                    color: Theme.of(context).focusColor),
-                labelText: "mot de passe",
-                labelStyle: TextStyle(color: Theme.of(context).focusColor),
-                suffixIcon: IconButton(
-                  onPressed: _togglePasswordView,
-                  icon: Icon(
-                    _isHidden ? Icons.visibility : Icons.visibility_off,
-                    color: Theme.of(context).focusColor,
-                  ),
-                ),
+    return FractionallySizedBox(
+      heightFactor: 0.88,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        //height: 600,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            TextFormField(
-              focusNode: fpasswordbis,
-              controller: _passwordBisInput,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Theme.of(context).canvasColor)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Theme.of(context).focusColor)),
-                prefixIcon: Icon(Icons.lock_outline,
-                    color: Theme.of(context).focusColor),
-                labelText: "mot de passe bis repetita",
-                labelStyle: TextStyle(color: Theme.of(context).focusColor),
-                suffixIcon: IconButton(
-                  onPressed: _togglePasswordView,
-                  icon: Icon(
-                    _isHidden ? Icons.visibility : Icons.visibility_off,
-                    color: Theme.of(context).focusColor,
-                  ),
-                ),
+              Text(
+                "Tout commence par un incipit",
+                style: Theme.of(context).textTheme.bodyText2,
               ),
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            TextFormField(
-              focusNode: fpicture,
-              controller: _pictureInput,
-              decoration: fieldDecoration(context, Icons.camera,
-                  "adresse url de votre photo de profil"),
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Theme.of(context).focusColor,
-                minimumSize: const Size(250, 50),
+              const SizedBox(
+                height: 20,
               ),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  if (_passwordInput.text == _passwordBisInput.text) {
-                    _signUpUser(_emailInput.text, _passwordInput.text,
-                        _pseudoInput.text, _pictureInput.text);
+              //Form field for the name
+              CustomFormField(
+                hintText: "pseudo",
+                focusNode: fpseudo,
+                textEditingController: _pseudoInput,
+                iconData: Icons.person,
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return "Merci d'indiquer un pseudo";
+                  } else if (val.length < 3) {
+                    return "Merci de choisir un pseudo de plus de trois caractères";
                   } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("mots de passe différents"),
-                      ),
-                    );
+                    return null;
                   }
-                }
-              },
-              child: const Text(
-                "S'INSCRIRE",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                },
+              ),
+              //Form Field for the mail
+              CustomFormField(
+                hintText: "courriel",
+                iconData: Icons.alternate_email,
+                focusNode: fmail,
+                textEditingController: _emailInput,
+                validator: (val) {
+                  if (!val!.isValidEmail) return 'Entrez un courriel valide';
+                },
+              ),
+
+              TextFormField(
+                focusNode: fpassword,
+                controller: _passwordInput,
+                obscureText: _isHidden,
+                validator: (val) {
+                  if (val!.length > 7) {
+                    if (val.isValidPassword) {
+                      return null;
+                    } else {
+                      print("password non valide");
+                      return "Votre mot de passe doit comporter au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial";
+                    }
+                  } else {
+                    return "Votre mot de passe doit comporter au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial";
+                  }
+                },
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock_outline,
+                      color: Theme.of(context).focusColor),
+                  labelText: "mot de passe",
+                  labelStyle: TextStyle(color: Theme.of(context).focusColor),
+                  suffixIcon: IconButton(
+                    onPressed: _togglePasswordView,
+                    icon: Icon(
+                      _isHidden ? Icons.visibility : Icons.visibility_off,
+                      color: Theme.of(context).focusColor,
+                    ),
+                  ),
+                  errorMaxLines: 3,
+                ),
+                // style: Theme.of(context).textTheme.bodyText2,
+              ),
+              TextFormField(
+                focusNode: fpasswordbis,
+                controller: _passwordBisInput,
+                validator: (val) {
+                  if (_passwordInput.text != val) {
+                    return "Les deux mots de passe sont différents";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  // enabledBorder: UnderlineInputBorder(
+                  //     borderSide:
+                  //         BorderSide(color: Theme.of(context).canvasColor)),
+                  // focusedBorder: UnderlineInputBorder(
+                  //     borderSide:
+                  //         BorderSide(color: Theme.of(context).focusColor)),
+                  prefixIcon: Icon(Icons.lock_outline,
+                      color: Theme.of(context).focusColor),
+                  labelText: "mot de passe bis repetita",
+                  labelStyle: TextStyle(color: Theme.of(context).focusColor),
+                  suffixIcon: IconButton(
+                    onPressed: _togglePasswordView,
+                    icon: Icon(
+                      _isHidden ? Icons.visibility : Icons.visibility_off,
+                      color: Theme.of(context).focusColor,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+              //Form Field for the picture
+              CustomFormField(
+                hintText: "adresse url de votre photo de profil",
+                focusNode: fpicture,
+                validator: (val) {
+                  if (val!.isValidImageUrl || val == "non") {
+                    return null;
+                  } else {
+                    return 'Url non valide.Y a-t-il un .png ou .jpg à la fin ? Si vous ne souhaitez pas ajouter de photo de profil, écrivez "non"';
+                  }
+                },
+                textEditingController: _pictureInput,
+                iconData: Icons.camera,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).focusColor,
+                  minimumSize: const Size(250, 50),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    if (_passwordInput.text == _passwordBisInput.text) {
+                      _signUpUser(_emailInput.text, _passwordInput.text,
+                          _pseudoInput.text, _pictureInput.text);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("mots de passe différents"),
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: const Text(
+                  "S'INSCRIRE",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  InputDecoration fieldDecoration(
-      BuildContext context, IconData icon, String labelText) {
-    return InputDecoration(
-        enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Theme.of(context).canvasColor)),
-        focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Theme.of(context).focusColor)),
-        errorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Theme.of(context).shadowColor),
-        ),
-        prefixIcon: Icon(icon, color: Theme.of(context).focusColor),
-        labelText: labelText,
-        labelStyle: TextStyle(color: Theme.of(context).focusColor),
-        errorStyle: TextStyle(color: Theme.of(context).shadowColor));
-  }
+  // InputDecoration fieldDecoration(
+  //     BuildContext context, IconData icon, String labelText) {
+  //   return InputDecoration(
+  //       enabledBorder: UnderlineInputBorder(
+  //           borderSide: BorderSide(color: Theme.of(context).canvasColor)),
+  //       focusedBorder: UnderlineInputBorder(
+  //           borderSide: BorderSide(color: Theme.of(context).focusColor)),
+  //       errorBorder: UnderlineInputBorder(
+  //         borderSide: BorderSide(color: Theme.of(context).shadowColor),
+  //       ),
+  //       prefixIcon: Icon(icon, color: Theme.of(context).focusColor),
+  //       labelText: labelText,
+  //       labelStyle: TextStyle(color: Theme.of(context).focusColor),
+  //       errorStyle: TextStyle(color: Theme.of(context).shadowColor));
+  // }
 }
---
+++ lib/shared/custom_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({
+    Key? key,
+    required this.hintText,
+    this.iconData,
+    this.focusNode,
+    this.textEditingController,
+    this.inputFormatters,
+    this.validator,
+  }) : super(key: key);
+  final String hintText;
+  final IconData? iconData;
+  final FocusNode? focusNode;
+  final TextEditingController? textEditingController;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        inputFormatters: inputFormatters,
+        validator: validator,
+        controller: textEditingController,
+        decoration: InputDecoration(
+            labelText: hintText, prefixIcon: Icon(iconData), errorMaxLines: 3),
+      ),
+    );
+  }
+}
+
+extension ExtString on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidPassword {
+    final passwordRegExp =
+        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+
+    return passwordRegExp.hasMatch(this);
+  }
+
+  bool get isValidImageUrl {
+    final imageUrlRegExp = RegExp(
+        r'^(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|jpeg|png))(?:\?([^#]*))?(?:#(.*))?');
+    return imageUrlRegExp.hasMatch(this);
+  }
+}
